d240de245f83bd96c9dab154d0dbabebf185fe9e
manasram <b.manasram@gmail.com>
2017-06-18 18:44:00 -0500

    Add files via upload
    Javascript code that contains the negative age, exactly 21, a perfect square of age and odd age.

--- javascript2.js
+++ javascript2.js
@@ -1,2 +1,16 @@
 var age= prompt("What is your age?");
-alert("You have lived "+ age*365 + " days so far");
+if (age<0){
+    alert("Negative age");
+}
+
+else if (age==="21") {
+    alert("Happy 21st Birthday!!");
+}
+
+else if (age%3==0) {
+    alert("Your age is odd.");
+}
+
+else if(age% Math.sqrt(age)===0) {
+    alert("Perfect Square");
+}

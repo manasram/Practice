11e358ab8216aee265a18cb3994bc60fc397ce42
manasram <b.manasram@gmail.com>
2017-06-18 20:44:01 -0500

    Add files via upload
    1.All numbers divisible by both 5 and 3
    2. All numbers between -10 and 19
    3. All  even numbers between 10 and 40
    4. All odd numbers between 300 and 333

---
+++ javascript3.js
@@ -0,0 +1,48 @@
+console.log("All numbers divisible by both 5 and 3");
+var num=5;
+
+
+while(num<=50){
+
+    if(num%3==0 && num%5==0){
+        console.log("The number is :" +num);
+        num++;
+    }
+    else{
+        num++;
+    }
+}
+
+console.log("-------------------------------------");
+console.log("All numbers between -10 and 19");
+var num=-10;
+
+
+while(num<=19){
+        console.log("The number is :" +num);
+        num++;
+}
+
+console.log("-------------------------------------");
+console.log("All  even numbers between 10 and 40")
+var num=10;
+
+
+while(num<=40){
+
+
+        console.log("The number is :" +num);
+        num+=2;
+    }
+
+console.log("-------------------------------------");
+console.log("All odd numbers between 300 and 333")
+var num=300;
+
+
+while(num<=333){
+
+
+        console.log("The number is :" +num);
+        num+=3;
+    }

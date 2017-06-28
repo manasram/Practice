77bc0574e3c79d2d065b796d0c08b04c98972b8d
manasram <b.manasram@gmail.com>
2017-06-27 20:50:53 -0500

    Add files via upload

---
+++ queryselector.js
@@ -0,0 +1,8 @@
+var tag = document.getElementById("first");
+console.log(tag);
+var tag1 = document.getElementsByClassName("special");
+console.log(tag1);
+var tag2= document.querySelector("#first");
+console.log(tag2);
+var tag3= document.querySelector(".special");
+console.log(tag3);
